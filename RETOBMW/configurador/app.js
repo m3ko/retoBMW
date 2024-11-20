3e2c1643c81bc8b152d77bd2b6523f073a65ff45
= <=>
2024-11-20 12:59:16 +0100

    funcion calcular precio configurador

--- RETOBMW/configurador/app.js
+++ RETOBMW/configurador/app.js
@@ -4,7 +4,10 @@ window.onload = obtenerMotores();
 window.onload = obtenerFrenos();
 window.onload = obtenerKitAerodinamicos();
 window.onload = obtenerLlantas();
-window.onload=obtenerSuspensiones();
+window.onload = obtenerSuspensiones();
+window.onload= definirPrecio();
+
+
 //  $(document).ready(function() {
 
 //     $('.color-choose input').on('click', function() {
@@ -60,22 +63,27 @@ async function imagenModelo() {
   });
 }
 
-async function obtenerFrenos() {
-  
-  const response = await fetch('../../Controlador/obtenerFrenosConfig.php');
-  const frenos = await response.json();
 
-  var botonesFrenos = document.querySelector("#frenos");
-  
-  frenos.forEach(freno => {
-    
-    botonesFrenos.innerHTML+=`<button class="button" onclick="selectButton(this)" value="${freno.id_freno}">${freno.tipo}</button>`;
+async function obtenerMotores() {
 
-  });
+  const response = await fetch('../../Controlador/obtenerMotoresConfig.php');
+  const motores = await response.json();
+
+
+
+  var botonesMotores = document.querySelector("#motor");
+
+  motores.forEach((motor, index) => {
+    if(index==0){
+      botonesMotores.innerHTML += ` <button class="button selected" onclick="selectButton(this)" value="${motor.id_motor}"">${motor.cilindrada}cc ${motor.caballos}cv ${motor.combustion}</button>`
+      
+    }else{
+    botonesMotores.innerHTML += ` <button class="button" onclick="selectButton(this)" value="${motor.id_motor}"">${motor.cilindrada}cc ${motor.caballos}cv ${motor.combustion}</button>`
+
+  }  });
 
 
 }
-
 async function obtenerKitAerodinamicos() {
 
   const response = await fetch('../../Controlador/obtenerKitAerodinamicosConfig.php');
@@ -83,48 +91,67 @@ async function obtenerKitAerodinamicos() {
 
   var botonesKits = document.querySelector("#kits");
 
-  kits.forEach(kit => {
+  kits.forEach((kit,index) => {
+    if(index==0){
+      botonesKits.innerHTML+=`<button class="button selected" onclick="selectButton(this)" value="${kit.id_kit}">${kit.nombre_kit}</button>`;
+
+    }else{
     botonesKits.innerHTML+=`<button class="button" onclick="selectButton(this)" value="${kit.id_kit}">${kit.nombre_kit}</button>`;
-  });
+}});
   
 }
+async function obtenerFrenos() {
+  
+  const response = await fetch('../../Controlador/obtenerFrenosConfig.php');
+  const frenos = await response.json();
 
-async function obtenerMotores() {
+  var botonesFrenos = document.querySelector("#frenos");
+  
+  frenos.forEach((freno, index) => {
+    if(index==0){
+      botonesFrenos.innerHTML+=`<button class="button selected" onclick="selectButton(this)" value="${freno.id_freno}">${freno.tipo}</button>`;
 
-  const response = await fetch('../../Controlador/obtenerMotoresConfig.php');
-  const motores = await response.json();
+    }else{
+    botonesFrenos.innerHTML+=`<button class="button" onclick="selectButton(this)" value="${freno.id_freno}">${freno.tipo}</button>`;
 
-  var botonesMotores = document.querySelector("#motor");
-
-  motores.forEach(motor => {
-
-    botonesMotores.innerHTML += ` <button class="button" onclick="selectButton(this)" value="${motor.id_motor}"">${motor.cilindrada}cc ${motor.caballos}cv ${motor.combustion}</button>`
-
-  });
+ } });
 
 
 }
-async function obtenerLlantas() {
-
-  const response = await fetch('../../Controlador/obtenerLlantasConfig.php');
-  const llantas = await response.json();
-  var botonesLlantas = document.querySelector("#llantas");
-
-  llantas.forEach(llanta => {
-    botonesLlantas.innerHTML += `<button class="button" onclick="selectButton(this)" value="${llanta.id_llanta}">${llanta.nombre_llanta}</button>`;
-  });
-}
-
 async function obtenerSuspensiones() {
 
   const response = await fetch('../../Controlador/obtenerSuspensionesConfig.php');
   const suspensiones = await response.json();
   var botonesSuspensiones = document.querySelector("#suspensiones");
 
-  suspensiones.forEach(suspension => {
+  suspensiones.forEach((suspension, index) => {
+    if(index==0){
+      botonesSuspensiones.innerHTML += `<button class="button selected" onclick="selectButton(this)" value="${suspension.id_suspension}">${suspension.nombre_suspension}</button>`;
+
+    }else{
     botonesSuspensiones.innerHTML += `<button class="button" onclick="selectButton(this)" value="${suspension.id_suspension}">${suspension.nombre_suspension}</button>`;
-  });
+}});
 }
+async function obtenerLlantas() {
+
+  const response = await fetch('../../Controlador/obtenerLlantasConfig.php');
+  const llantas = await response.json();
+  var botonesLlantas = document.querySelector("#llantas");
+  
+  llantas.forEach((llanta,index) => {
+    if(index==0){
+      botonesLlantas.innerHTML += `<button class="button selected" onclick="selectButton(this)" value="${llanta.id_llanta}">${llanta.nombre_llanta}</button>`;
+
+    }else{
+    botonesLlantas.innerHTML += `<button class="button" onclick="selectButton(this)" value="${llanta.id_llanta}">${llanta.nombre_llanta}</button>`;
+}});
+  console.log("estoy aki");
+  var botonesLlantas2 = botonesLlantas.parentNode("button");
+  console.log(botonesLlantas2.firstChild);
+  botonesLlantas2.firstChild.classList.add('selected');
+}
+
+
 
 function selectButton(button) {
   // Obtener la fila del botón seleccionado
@@ -136,6 +163,28 @@ function selectButton(button) {
   }
   // Marcar el botón seleccionado
   button.classList.add('selected');
+
+  
+}
+
+function sumarprecio(){
+
+  var precioMotor=document.querySelectorAll("#motor>button");
+  
+
+}
+
+async function definirPrecio(){
+
+  var precioMotor = document.querySelector("#motor");
+  var boton = await precioMotor.getElementsByClassName('selected')[0].value;
+  console.log(boton);
+  
+  
+  console.log("el primero es:"+boton[0]);
+  
+
+
 }
 
 
